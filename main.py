f2ad7261e2169cab94c73b504d42c4a34bcd34e6
TechNus09 <59443272+TechNus09@users.noreply.github.com>
2021-11-23 20:03:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 import os, binascii
 import pytube
@@ -6,18 +7,36 @@ from pytube import YouTube
 
 bot = commands.Bot(command_prefix = '$')
 filedirectory = os.getcwd()
-bot.remove_command('help')
 
 
+"""
+client = discord.client()
+already called client, no need to call client, change all the client's mentions in the code to 'client'
+"""
 
 @bot.event
 async def on_ready():
     print("We have logged in as {0.user}".format(bot))
 
+""""@bot.event
+async def on_message(message):
+    #Doesn't reply to itself
+    if message.author == bot.user:
+        return
+    
+    #Command help
+    if message.content.startswith("$help"):
+        await message.channel.send("Use '$youtube [link]' to download it as an mp3. Just make sure the embed isn't hidden!")"""
+
+"""can change the previous lines to just : """
 @bot.command()
 async def help(ctx):
-    await ctx.send("Use $ before a youtube link to download it as an mp3. Just make sure the embed isn't hidden!")
+    await ctx.send("Use '$youtube [link]' to download it as an mp3. Just make sure the embed isn't hidden!")
 
+
+"""
+@bot.event should be changed to @bot.command() so you can interact with the bot
+"""
 @bot.command()
 async def youtube(ctx, link, filetype):
     await ctx.send('Please wait a moment...')
@@ -31,7 +50,7 @@ async def youtube(ctx, link, filetype):
         # finds the first stream in the list that is progressive (meaning it has both video and audio) and is an mp4 file
         video = url.streams.filter(progressive = True, file_extension = 'mp4').first()
         await ctx.send('Successfully found video. Downloading...')
-            
+        
         # and finally it sends the video file thru discord
         video.download(filedirectory, filename=fname + '.mp4')
         await ctx.send(file=discord.File(f'{filedirectory}/{fname}.mp4'))
@@ -39,36 +58,10 @@ async def youtube(ctx, link, filetype):
         # finds the first stream that is audio. this is guaranteed to always be a mp3 file.
         video = url.streams.filter(only_audio=True).first()
         await ctx.send('Successfully found audio. Downloading...')
-
         # and finally it sends the audio file thru discord
         video.download(filedirectory, filename=fname + '.mp3')
         await ctx.send(file=discord.File(f'{filedirectory}/{fname}.mp3'))
     else:
         await ctx.send("Not a supported filetype!!!")
 
-
-
 bot.run(os.environ.get("TOKEN"))
-
-""" to ping the author:
-await message.channel.send("{}".format(message.author.mention)
-
-to ping another user:
-await message.channel.send("<@![user id]>")
-
-to add an emoji:
-await message.channel.send("<:[emoji name]:[emoji id]>")
-"""
-
-"""
-async def on_message(message):
-
-
-    link = message.content
-    video = YouTube(link)
-    stream = video.streams.get_highest_resolution()
-    stream.download()
-    with open(stream.download, 'rb') as f:
-        picture = discord.File(f)
-        await message.channel.send(file=discord.File(stream.download))
-"""
